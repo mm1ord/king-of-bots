73925eba180271edefb5b28d542fbe437a9c7ccc
mm1ord <1090485174@qq.com>
2022-10-06 20:12:40 +0800

    创建了地图

--- web/src/views/pk/PkindexView.vue
+++ web/src/views/pk/PkindexView.vue
@@ -1,16 +1,16 @@
 <template>
-    <ContentField>
-        对战
-    </ContentField>
+    <PlayGround>
+
+    </PlayGround>
 </template>
 
 
 <script>
-import ContentField from '../../components/ContentField.vue'
+import PlayGround from '../../components/PlayGround.vue'
 
 export default {
     components: {
-        ContentField
+        PlayGround,
     },
 }
 
