704cfde50cc87d731de4f6baa86e267cf2aeedfa
mm1ord <1090485174@qq.com>
2022-10-10 09:12:48 +0800

    蛇可以动了

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,33 +1,28 @@
 import { AcGameObject } from "./AcGameObject";
-import { Snake } from "./Snake";
 import { Wall } from "./Wall";
+import { Snake } from "./Snake";
 
 export class GameMap extends AcGameObject {
     constructor(ctx, parent) {
         super();
+
         this.ctx = ctx;
         this.parent = parent;
         this.L = 0;
 
         this.rows = 13;
         this.cols = 14;
+
+        this.walls = [];
+
         this.inner_walls_count = 20;
         this.walls = [];
 
         this.snakes = [
-            new Snake({
-                id: 0,
-                color: "#4876EC",
-                r: this.rows - 2,
-                c: 1,
-            }, this),
-            new Snake({
-                id: 1,
-                color: "#F94848",
-                r: 1,
-                c: this.cols - 2,
-            }, this),
-        ]
+            new Snake({ id: 0, color: "#4876EC", r: this.rows - 2, c: 1 }, this),
+            new Snake({ id: 1, color: "#F94848", r: 1, c: this.cols - 2 }, this),
+        ];
+
     }
 
     check_connectivity(g, sx, sy, tx, ty) {
@@ -43,7 +38,7 @@ export class GameMap extends AcGameObject {
         return false;
     }
 
-    create_walls() {
+    crete_walls() {
         const g = [];
         for (let r = 0; r < this.rows; r++) {
             g[r] = [];
@@ -51,6 +46,7 @@ export class GameMap extends AcGameObject {
                 g[r][c] = false;
             }
         }
+
 
         //给四周加上障碍物
         for (let r = 0; r < this.rows; r++) {
@@ -61,18 +57,19 @@ export class GameMap extends AcGameObject {
             g[0][c] = g[this.rows - 1][c] = true;
         }
 
-        for (let i = 0; i < this.inner_walls_count; i++) {
+        //创建随机障碍物
+        for (let i = 0; i < this.inner_walls_count / 2; i++) {
             for (let j = 0; j < 1000; j++) {
                 let r = parseInt(Math.random() * this.rows);
                 let c = parseInt(Math.random() * this.cols);
                 if (g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c]) continue;
-                if (r == this.rows - 2 && c == 1 || r == 1 && c == this.cols - 2) {
+                if (r == this.rows - 2 && c == 1 || r == 1 && c == this.cols - 2)
                     continue;
-                }
                 g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
                 break;
             }
         }
+
         const copy_g = JSON.parse(JSON.stringify(g));
         if (!this.check_connectivity(copy_g, this.rows - 2, 1, 1, this.cols - 2)) return false;
 
@@ -83,44 +80,34 @@ export class GameMap extends AcGameObject {
                 }
             }
         }
-
         return true;
     }
 
     add_listening_events() {
         this.ctx.canvas.focus();
+
         const [snake0, snake1] = this.snakes;
-        this.ctx.canvas.addEventListener('keydown', e => {
-            if (e.key === 'w') {
-                snake0.set_direction(0);
-            } else if (e.key === 'd') {
-                snake0.set_direction(1);
-            } else if (e.key === 's') {
-                snake0.set_direction(2);
-            } else if (e.key === 'a') {
-                snake0.set_direction(3);
-            } else if (e.key === 'ArrowUp') {
-                snake1.set_direction(0);
-            } else if (e.key === 'ArrowRight') {
-                snake1.set_direction(1);
-            } else if (e.key === 'ArrowDown') {
-                snake1.set_direction(2);
-            } else if (e.key === 'ArrowLeft') {
-                snake1.set_direction(3);
-            }
+        this.ctx.canvas.addEventListener("keydown", e => {
+            if (e.key === 'w') snake0.set_direction(0);
+            else if (e.key === 'd') snake0.set_direction(1);
+            else if (e.key === 's') snake0.set_direction(2);
+            else if (e.key === 'a') snake0.set_direction(3);
+            else if (e.key === 'ArrowUp') snake1.set_direction(0);
+            else if (e.key === 'ArrowRight') snake1.set_direction(1);
+            else if (e.key === 'ArrowDown') snake1.set_direction(2);
+            else if (e.key === 'ArrowLeft') snake1.set_direction(3);
         });
     }
 
 
+
     start() {
-        for (let i = 0; i < 1000; i++) {
-            if (this.create_walls())
+        for (let i = 0; i < 1000; i++)
+            if (this.crete_walls())
                 break;
-        }
 
+        this.add_listening_events();
     }
-
-
 
     update_size() {
         this.L = parseInt(Math.min(this.parent.clientWidth / this.cols, this.parent.clientHeight / this.rows));
@@ -128,7 +115,7 @@ export class GameMap extends AcGameObject {
         this.ctx.canvas.height = this.L * this.rows;
     }
 
-    check_ready() {
+    check_ready() { //判断两条蛇是否都准备好下一回合了
         for (const snake of this.snakes) {
             if (snake.status !== "idle") return false;
             if (snake.direction === -1) return false;
@@ -136,7 +123,29 @@ export class GameMap extends AcGameObject {
         return true;
     }
 
-    next_step() {
+
+    check_valid(cell) {  // 检测目标位置是否合法：没有撞到两条蛇的身体和障碍物
+        for (const wall of this.walls) {
+            if (wall.r === cell.r && wall.c === cell.c)
+                return false;
+        }
+
+        for (const snake of this.snakes) {
+            let k = snake.cells.length;
+            if (!snake.check_tail_increasing()) {  // 当蛇尾会前进的时候，蛇尾不要判断
+                k--;
+            }
+            for (let i = 0; i < k; i++) {
+                if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    next_step() { //让两条蛇进入下一回合
         for (const snake of this.snakes) {
             snake.next_step();
         }
@@ -144,7 +153,6 @@ export class GameMap extends AcGameObject {
 
     update() {
         this.update_size();
-        this.add_listening_events();
         if (this.check_ready()) {
             this.next_step();
         }
@@ -152,9 +160,7 @@ export class GameMap extends AcGameObject {
     }
 
     render() {
-        // this.ctx.fillStyle = 'green';
-        // this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-        const color_even = "#AAD751", color_odd = "#A2D149";
+        const color_even = "#AAD752", color_odd = "#A2D048";
         for (let r = 0; r < this.rows; r++) {
             for (let c = 0; c < this.cols; c++) {
                 if ((r + c) % 2 == 0) {
@@ -166,4 +172,6 @@ export class GameMap extends AcGameObject {
             }
         }
     }
+
+
 }
