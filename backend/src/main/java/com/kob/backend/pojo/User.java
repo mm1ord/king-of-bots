102cce7c13003d6861013756baa2e2d8fe17a5da
mm1ord <1090485174@qq.com>
2022-11-03 15:16:52 +0800

    bot界面做好了

--- backend/src/main/java/com/kob/backend/pojo/User.java
+++ backend/src/main/java/com/kob/backend/pojo/User.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-    @TableId(type = IdType.ASSIGN_ID)
+    @TableId(value = "id",type = IdType.AUTO)
     private Integer id;
     private String username;
     private String password;
